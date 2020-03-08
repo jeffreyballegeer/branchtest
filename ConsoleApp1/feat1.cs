c5fb93013795056b39de925aea91c02c50fc1c17
ConsoleFriend <jeffrey.ballegeer@student.howest.be>
2020-03-08 17:20:11 +0100

    add a line and new branch featadd voegt gewoon ieuw lijntje toe

--- ConsoleApp1/feat1.cs
+++ ConsoleApp1/feat1.cs
@@ -7,5 +7,6 @@ namespace ConsoleApp1
     class feat1
     {//class toegevoegd onder branch feat_1
         //toegevoegd als 2e commit in feat_1
+        //toegevoegd in nieuwe subbranch featadd (branch van feat°1)
     }
 }
